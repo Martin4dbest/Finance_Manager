713a1a609b0fb05d0dc2051f4e866a06d9e86cfb
Martin Agoha <martin4dtruth@gmail.com>
2024-08-31 09:26:49 +0100

    updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///finance.db"
-app.config["SECRET_KEY"] = "2160d9f2965f252a882c65054457caa4c87e48372b5d7227"  # Your secret key
+app.config["SECRET_KEY"] = "2160d9f2965f252a882c65054457caa4c87e48372b5d7227"  # Your secret key is here
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
